61fc25ff3b8125c7a4ace8094dc6a8ef65b1660e
chet rungruang <chetster@gmail.com>
2023-10-14 14:32:59 -1000

    initial commit

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const geolib = require('geolib');
+
+const app = express();
+const port = 8080;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/upload', upload.single('jsonFile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const filePath = req.file.path;
+
+  // Read and parse the uploaded JSON file
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error reading the file.');
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      // Filter and extract coordinates of type "Point" with class "Marker"
+      const points = jsonData.features.filter(
+        feature => feature.geometry.type === 'Point' &&
+                    feature.properties.class === 'Marker'
+      );
+
+      // Calculate bearing and distance for each combination of coordinates and titles
+      const result = calculateBearingAndDistance(points);
+
+      // Format the result as a JSON response with new lines
+      const formattedResponse = JSON.stringify(result, null, 2);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.send(formattedResponse);
+    } catch (error) {
+      return res.status(400).send('Invalid JSON format.');
+    }
+  });
+});
+
+// Calculate bearing and distance for each combination of coordinates and titles
+function calculateBearingAndDistance(points) {
+  const result = {};
+
+  for (const start of points) {
+    const from = start.properties.title;
+    result[from] = [];
+
+    for (const next of points) {
+      if (start !== next) {
+        const bearing = geolib.getRhumbLineBearing(
+          start.geometry.coordinates,
+          next.geometry.coordinates
+        );
+        const distance = geolib.getDistance(
+          start.geometry.coordinates,
+          next.geometry.coordinates
+        );
+        const to = next.properties.title;
+
+        result[from].push({
+          from,
+          to,
+          bearing: Number(bearing.toFixed(2)), // Round bearing to 2 decimal places
+          distance,
+        });
+      }
+    }
+  }
+
+  return result;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
